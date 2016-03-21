93fa85cfb5c3cc311c21dfa199eb28c39bfccd22
callihiggins <calli@nyu.edu>
2016-03-21 12:24:00 -0400

    Week 1 and 2 Examples

---
+++ Week 2/script.js
@@ -0,0 +1,12 @@
+function hide_nav(){
+
+/*When an HTML document is loaded into a web browser, it becomes a document object.
+The document object is the root node of the HTML document and the "owner" of all other nodes:
+(element nodes, text nodes, attribute nodes, and comment nodes). */
+	document.getElementById("the_nav").style.background="#000";
+	document.getElementsByClassName("test-btn")[0].innerHTML="Why is the nav black now?"
+	console.log("hi");
+	
+	//Turn this on and off to pause the code and inspect it
+	//debugger;
+}
